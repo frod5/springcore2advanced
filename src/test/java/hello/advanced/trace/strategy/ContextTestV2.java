8c845bd55f48f4b06962e6bdee5f8b1d76510ba5
sunwooKim <frod5@kakao.com>
2023-04-20 16:17:20 +0900

    스프링 핵심 원리2 - 전략 패턴(Strategy) - 예제3(파라미터로 전략전달 방식), 예제2는 필드주입방식

---
+++ src/test/java/hello/advanced/trace/strategy/ContextTestV2.java
@@ -0,0 +1,55 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV2;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextTestV2 {
+
+    /**
+     * 전략 패턴 사용
+     * context와 strategy를 선 조립 후 실행이 아닌, 파라미터로 전략을 전달.
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+    /**
+     * 전략 패턴 사용 익명 내부 클래스
+     * context와 strategy를 선 조립 후 실행이 아닌, 파라미터로 전략을 전달.
+     */
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직1 실행");
+            }
+        });
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직2 실행");
+            }
+        });
+    }
+
+    /**
+     * 전략 패턴 사용 익명 내부 클래스2 람다사용
+     * context와 strategy를 선 조립 후 실행이 아닌, 파라미터로 전략을 전달.
+     */
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비지니스 로직1 실행"));
+        context.execute(() -> log.info("비지니스 로직2 실행"));
+    }
+}
